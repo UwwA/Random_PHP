e2ce419fcdb5121e162da32f8388bfceaef9d591
Anton Gärdälv <anton@gardalv.se>
2015-08-07 16:55:34 +0200

    Fixed indention

--- upload.php
+++ upload.php
@@ -10,19 +10,18 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 // Check if file already exists
 if (file_exists($target_file)) {
-
-    $uploadOk = 0;
+	$uploadOk = 0;
 	$_SESSION['upload'] = "Sorry, file already exists.";
 }
 // Check if file size is less then 5MB
 if ($_FILES["fileToUpload"]["size"] > 5242880) {
 	$_SESSION['upload'] = "Sorry, your file is too large.";
-    $uploadOk = 0;
+	$uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "pdf" ) {
 	$_SESSION['upload'] = "Sorry, only PDF files are allowed.";
-    $uploadOk = 0;
+	$uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
@@ -40,7 +39,7 @@ if ($uploadOk == 0) {
 		}
     } else {
        			$_SESSION['upload'] = "Unknown error.";
-				header('Location: exercises.php');
+			header('Location: exercises.php');
     }
 }
 ?>
